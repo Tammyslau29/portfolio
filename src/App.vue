c478ffe3c38d4288b3ac6dde71411ed418115134
Tammy <tammyslau29@gmail.com>
2019-05-05 22:06:08 -0700

    initial gh branch

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
   <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/GreyWhiteLogo.png">
-    <HelloWorld msg="Hi There."/>
+    <HelloWorld msg="Hi There"/>
   </div>
 </template>
 
